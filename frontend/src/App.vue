1910e49582fb49dffb28aa90f14ff84f8fc39a67
신유빈 <youbin1294@naver.com>
2020-10-20 12:49:49 +0900

    S03P31A301-14 | updated feature | vuex, vuetify 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
+  <v-app>
     <div id="nav">
       <router-link to="/shop">장보기 화면 보기</router-link> |
       <router-link to="/user">유저화면</router-link>
     </div>
     <router-view />
-  </div>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
