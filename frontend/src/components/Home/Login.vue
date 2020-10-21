d4af36740b140e6a17fd7688bf4648b2c8fc25c1
신유빈 <youbin1294@naver.com>
2020-10-21 11:29:21 +0900

    S03P31A301-15 | added feature | 회원가입 폼 안성

---
+++ frontend/src/components/Home/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <h1>로그인</h1>
+    <v-text-field
+      v-model="loginData.userId"
+      label="아이디"
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="loginData.password"
+      type="password"
+      label="비밀번호"
+      required
+    ></v-text-field>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      loginData: {
+        userId: "",
+        password: "",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
