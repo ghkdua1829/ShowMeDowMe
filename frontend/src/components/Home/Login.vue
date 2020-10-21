6ed55cf98da37ac3bc5551d603618dea19f927cb
신유빈 <youbin1294@naver.com>
2020-10-21 13:21:09 +0900

    S03P31A301-32 | updated sytle | 로그인 화면 scss 추가

--- frontend/src/components/Home/Login.vue
+++ frontend/src/components/Home/Login.vue
@@ -5,29 +5,34 @@
       src="https://user-images.githubusercontent.com/60081201/96660569-e562cb80-1384-11eb-9b19-107de574a592.PNG"
       alt="쇼우미도우미 로고"
     />
-    <v-text-field
-      v-model="loginData.userId"
-      label="아이디"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="loginData.password"
-      type="password"
-      label="비밀번호"
-      required
-    ></v-text-field>
-    <v-btn
-      width="100%"
-      rounded
-      dark
-      color="teal"
-      @click="$router.push({ path: '/perparation' })"
-      >로그인</v-btn
-    >
+    <div class="login">
+      <v-text-field
+        v-model="loginData.userId"
+        label="아이디"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="loginData.password"
+        type="password"
+        label="비밀번호"
+        required
+      ></v-text-field>
+      <v-btn
+        class="login-btn"
+        width="60%"
+        rounded
+        dark
+        color="teal"
+        @click="$router.push({ path: '/perparation' })"
+        >로그인</v-btn
+      >
+    </div>
   </v-container>
 </template>
 
 <script>
+import "@/assets/css/components/Home/login.scss";
+
 export default {
   name: "Login",
   data() {
