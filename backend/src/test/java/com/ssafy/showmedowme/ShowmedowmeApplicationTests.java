209fbf288f6edc0c80db06242e811f6474725043
양동찬 <csyang12370@gmail.com>
2020-10-21 10:47:00 +0900

    S03P31A301-15 | added feature | Spring 환경생성

---
+++ backend/src/test/java/com/ssafy/showmedowme/ShowmedowmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.showmedowme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowmedowmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
