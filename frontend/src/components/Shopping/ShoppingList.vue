3ad339e5263a0da2aff8b17d0189a3c89a4e14e6
신유빈 <youbin1294@naver.com>
2020-10-27 11:06:34 +0900

    S03P31A301-41 | updated feature | 쇼핑리스트 틀 생성

--- frontend/src/components/Shopping/ShoppingList.vue
+++ frontend/src/components/Shopping/ShoppingList.vue
@@ -1,10 +1,234 @@
 <template>
-  <div>쇼핑 리스트 보여줄 화면</div>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    sort-by="amount"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn dark class="mb-2" v-bind="attrs" v-on="on"
+              >직접추가하기
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Dessert name"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.amount"
+                      label="amount"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.price"
+                      label="price (g)"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
+              <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="headline"
+              >Are you sure you want to delete this item?</v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete"
+                >Cancel</v-btn
+              >
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                >OK</v-btn
+              >
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot: item.actions="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize"> Reset </v-btn>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 export default {
   name: "ShoppingList",
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        text: "제품",
+        align: "start",
+        sortable: false,
+        value: "name",
+      },
+      { text: "수량", value: "amount" },
+      { text: "가격", value: "price" },
+      { text: "편집", value: "actions", sortable: false },
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      제품: "",
+      수량: 0,
+      가격: 0,
+    },
+    defaultItem: {
+      제품: "",
+      수량: 0,
+      가격: 0,
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      this.desserts = [
+        {
+          name: "Frozen Yogurt",
+          amount: 159,
+          price: 6.0,
+        },
+        {
+          name: "Ice cream sandwich",
+          amount: 237,
+          price: 9.0,
+        },
+        {
+          name: "Eclair",
+          amount: 262,
+          price: 16.0,
+        },
+        {
+          name: "Cupcake",
+          amount: 305,
+          price: 3.7,
+        },
+        {
+          name: "Gingerbread",
+          amount: 356,
+          price: 16.0,
+        },
+        {
+          name: "Jelly bean",
+          amount: 375,
+          price: 0.0,
+        },
+        {
+          name: "Lollipop",
+          amount: 392,
+          price: 0.2,
+        },
+        {
+          name: "Honeycomb",
+          amount: 408,
+          price: 3.2,
+        },
+        {
+          name: "Donut",
+          amount: 452,
+          price: 25.0,
+        },
+        {
+          name: "KitKat",
+          amount: 518,
+          price: 26.0,
+        },
+      ];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.desserts.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      } else {
+        this.desserts.push(this.editedItem);
+      }
+      this.close();
+    },
+  },
 };
 </script>
 
