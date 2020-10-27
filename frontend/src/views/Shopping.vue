58bfb31f9bb3cd185bc0383d1c9c2eeefb944fc7
신유빈 <youbin1294@naver.com>
2020-10-27 10:46:55 +0900

    S03P31A301-41 | updated sytle | 메모, 소요시간 header부분으로 수정

--- frontend/src/views/Shopping.vue
+++ frontend/src/views/Shopping.vue
@@ -1,28 +1,31 @@
 <template>
-  <div>
-    <h1>장보기 화면</h1>
-    <ShoppingMemo />
-    <ShoppingReceipe />
-    소요시간:
+  <div class="shopping">
+    <h1>장보는 중</h1>
+    <v-row>
+      <v-col cols="2">
+        <ShoppingMemo />
+      </v-col>
+      <v-col> 소요시간: </v-col>
+    </v-row>
     <ShoppingList />
-    <v-btn @click="$router.push({ path: '/camera'})">
-    물건등록
-    </v-btn>
+    <!-- <ShoppingReceipe /> -->
+    <v-btn @click="$router.push({ path: '/camera' })"> 물건등록 </v-btn>
     결재금액:
     <router-link :to="{ path: '/report' }">완료</router-link>
   </div>
 </template>
 
 <script>
+import "@/assets/css/views/shopping.scss";
 import ShoppingList from "@/components/Shopping/ShoppingList";
 import ShoppingMemo from "@/components/Shopping/ShoppingMemo";
-import ShoppingReceipe from "@/components/Shopping/ShoppingRecipe";
+// import ShoppingReceipe from "@/components/Shopping/ShoppingRecipe";
 
 export default {
   name: "Shopping",
   components: {
     ShoppingMemo,
-    ShoppingReceipe,
+    // ShoppingReceipe,
     ShoppingList,
   },
 };
