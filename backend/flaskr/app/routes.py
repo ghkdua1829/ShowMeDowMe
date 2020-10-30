e201f64fa94bccd127de9da449b293972aa81aa0
linkcoder-py <rnjs213@gmail.com>
2020-10-30 11:45:15 +0900

    updated feature | flask-server에 ocr 기능 추가

--- backend/flaskr/app/routes.py
+++ backend/flaskr/app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from app.Ocr import hello
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return "Hello, World!"
+
+@app.route('/postImage', methods=['POST'])
+def imageOcr():
+	hello()
