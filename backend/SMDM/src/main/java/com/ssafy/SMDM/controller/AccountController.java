4af148dfafdeeff0affbb08d2666a4554718419f
unknown <csyang12370@gmail.com>
2020-11-02 07:20:45 +0900

    JPA 연동및 기타기능들 구현

--- backend/SMDM/src/main/java/com/ssafy/SMDM/controller/AccountController.java
+++ backend/SMDM/src/main/java/com/ssafy/SMDM/controller/AccountController.java
@@ -1,41 +1,85 @@
 package com.ssafy.SMDM.controller;
 
+import com.ssafy.SMDM.dto.User;
+import com.ssafy.SMDM.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.ssafy.SMDM.repository.User;
-import com.ssafy.SMDM.repository.UserRepo;
-import com.ssafy.SMDM.service.JWTService;
 
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class AccountController {
-
     @Autowired
-    private UserRepo userrepo;
+    UserService userService;
 
-    @Autowired
-    private JWTService jwtService;
-
+    //로그인부분 추후에 JWT , Spring Sercurity 필요
     @PostMapping("/login")
     public Object login(@RequestBody User user){
-
-        ResponseEntity response = null;
-        User user1 = this.userrepo.findUSER
-                (user.getUser_id(),user.getUser_pw());
-        if(user1 != null) {
-            System.out.println("login");
-            //jwt 키 생성
-            String Jwttoken = jwtService.create("user_id",user.getUser_id(),user.getUser_birth());
-            response = new ResponseEntity(Jwttoken, HttpStatus.OK);
-            return ResponseEntity.ok(Jwttoken);
-        } else{
-            response = new ResponseEntity("not found", HttpStatus.NOT_FOUND);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("not correct");
+        Optional<User> u = userService.findByUserIdAndUserPw(user.getUserid(), user.getUserpw());
+        if(u.isPresent()){
+            //로그인 완료부분
+            return ResponseEntity.ok();
+        }else{
+            //로그인 실패부분
+            return ResponseEntity.status(HttpStatus.NOT_FOUND);
         }
     }
 
+    //회원가입
+    @PostMapping("/users")
+    public Object SubmitUser(@RequestBody User user){
+        return new ResponseEntity<User>(userService.save(user),HttpStatus.OK);
+    }
 
+    //id 중복체크
+    @PostMapping("/users/id")
+    public Object Idcheck(@RequestBody String id){
+        Optional<User> u = userService.findByUserId(id);
+        if(u.isPresent()) {
+            //아이디가 존재
+            return ResponseEntity.status(HttpStatus.NOT_FOUND);
+        }else{
+            return ResponseEntity.status(HttpStatus.OK);
+        }
+    }
+
+    //회원삭제
+    @DeleteMapping("/users")
+    public Object DeleteUser(@PathVariable String id){
+        userService.deleteByUserId(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    //회원정보 가져오기
+    @GetMapping("/users")
+    public Object GetUser(@RequestParam String id){
+        Optional<User> u = userService.findByUserId(id);
+        return new ResponseEntity<User>(u.get(),HttpStatus.OK);
+    }
+    
+    //회원정보수정
+    @PutMapping("/users")
+    public Object UpdateUser(@PathVariable String id, User Muser){
+        userService.updateByUserId(id,Muser);
+        return new ResponseEntity<User>(Muser,HttpStatus.OK);
+    }
+
+    //메모작성
+    @PutMapping("/users/memo")
+    public Object UpdateMemo(@PathVariable String id, String memo){
+        userService.updateByMemo(id,memo);
+        return new ResponseEntity<String>(memo,HttpStatus.OK);
+
+    }
+
+    //메모확인
+    @GetMapping("/users/memo")
+    public Object GetMemo(@RequestParam("userid") String id){
+        Optional<User> u = userService.findByUserId(id);
+
+        return new ResponseEntity<String>(u.get().getUserMemo(),HttpStatus.OK);
+    }
 }
--- backend/SMDM/src/main/java/com/ssafy/SMDM/service/JWTService.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.SMDM.service;
-
-public interface JWTService {
-    public <T> String create(String key, T data, String subject);
-
-}
