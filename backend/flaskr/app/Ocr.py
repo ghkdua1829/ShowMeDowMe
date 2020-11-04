e543ae409541dda4ace518b6344a80ddd2c62b15
Kim Chan Young <ghkdua059@gmail.com>
2020-11-04 16:19:28 +0900

    S03P31A301-86 | added feature | 가격표 인식

--- backend/flaskr/app/Ocr.py
+++ backend/flaskr/app/Ocr.py
@@ -1,6 +1,7 @@
 """Detects text in the file."""
 from google.cloud import vision_v1 as vision
 import io
+import re
 from flask import Flask, request
 from PIL import Image
 
@@ -15,18 +16,37 @@ def hello():
 
     image = vision.types.Image(content=content)
 
-    price_candidate = []
-    card_numbera_candidate = []
-    date_candidate = []
+    result = ["", ""]
 
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    print('Texts:')
+    # -*- Encoding:UTF-8 -*- #
+    print(texts[0].description.split("\n"))
+    textArr = texts[0].description.split("\n")
+    # s = '123abc.-30=*/-'
 
-    for text in texts:
-        content = text.description
-        content = content.replace(',', '')
-        print('\n"{}"'.format(content))
+    for text in textArr:
+        if text.find('.') != -1 or text.find(',') != -1:
+            i = int(re.findall('\d+', text)[0])
+            strNum = "".join(re.findall('\d+', text))
+            if 0 < int(strNum) < 500000:
+                print("후보 가격 !! : " + str(strNum))
+                result[1] = strNum
+
+        #
+        hangul = re.compile('[^ ㄱ-ㅣ가-힣]+')  # 한글과 띄어쓰기를 제외한 모든 글자
+        hangulResult = hangul.sub('', text)  # 한글과 띄어쓰기를 제외한 모든 부분을 제거
+        if len(hangulResult) > 3:
+            if text.find('가능') == -1 and text.find('개당') == -1 and text.find('g당') == -1 and text.find('기간') == -1 and text.find('추천') == -1:
+                print("후보 물건 이름!!! : " + text)
+                result[0] = text
+
+    # print('Texts:')
+    #
+    # for text in texts:
+    #     content = text.description
+    #     content = content.replace(',', '')
+    #     print('\n"{}"'.format(content))
 
     if response.error.message:
         raise Exception(
@@ -34,7 +54,36 @@ def hello():
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
 
-    return str(texts)
+    return str(str(result[0]) + " , " + str(result[1]))
+
+
+def detect_text(path):
+    """Detects text in the file."""
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                     for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
 
 
 def image_to_byte_array(image: Image):
