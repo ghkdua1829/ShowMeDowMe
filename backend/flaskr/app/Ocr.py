a6d03caafd432ef0bc8ff6f370fbcb37254c00c0
Kim Chan Young <ghkdua059@gmail.com>
2020-11-05 10:38:41 +0900

    S03P31A301-86 | added feature | 가격표 인식 정교화 작업

--- backend/flaskr/app/Ocr.py
+++ backend/flaskr/app/Ocr.py
@@ -22,28 +22,47 @@ def hello():
     texts = response.text_annotations
     # -*- Encoding:UTF-8 -*- #
     print(texts[0].description.split("\n"))
-    textArr = texts[0].description.split("\n")
+    text_arr = texts[0].description.split("\n")
     # s = '123abc.-30=*/-'
 
-    for text in textArr:
-        # if text.find('.') != -1 or text.find(',') != -1:
-        strNum = "".join(re.findall('\d+', text))
-        print("[" + strNum + "]")
-        if len(strNum) != 0:
-            if 100 < int(strNum) < 500000:
-                print("후보 가격 !! : " + str(strNum))
-                result[1] = strNum
+    limit_texts = ['가능', '원산지', '표기', '용기', '개당', 'g당', '기간', '추천', ',', '단위', '가격', '카드', '할인', '마트', '박스', '그대로',
+                   '온라인몰', '행사']
+    hangul = re.compile('[^ ㄱ-ㅣ가-힣]+')  # 한글과 띄어쓰기를 제외한 모든 글자
+
+    for text in text_arr:
+        str_num = "".join(re.findall('\d+', text))
+        hangul_result = hangul.sub('', text)  # 한글과 띄어쓰기를 제외한 모든 부분을 제거
+        print("[" + str_num + "]")
+        if len(str_num) != 0:
+
+            if len(hangul_result) < 3 and 100 < int(str_num) < 500000 and text.find('/') == -1:
+                print("후보 가격 !! : " + str(str_num) + " , " + hangul_result)
+                result[1] = str_num
 
         #
-        hangul = re.compile('[^ ㄱ-ㅣ가-힣]+')  # 한글과 띄어쓰기를 제외한 모든 글자
-        hangulResult = hangul.sub('', text)  # 한글과 띄어쓰기를 제외한 모든 부분을 제거
-        if len(hangulResult) > 3:
-            if text.find('가능') == -1 and text.find('원산지') == -1 and text.find('표기') == -1 and text.find(
-                    '용기') == -1 and text.find(
-                    '개당') == -1 and text.find('g당') == -1 and text.find('기간') == -1 and text.find('추천') == -1 and text.find(',') == -1:
+
+        if len(hangul_result) > 3:
+            token = False
+
+            for limit in limit_texts:
+                if text.find(limit) != -1:
+                    token = True
+                    break
+
+            if token is False:
                 print("후보 물건 이름!!! : " + text)
-                # if result[0][0]=='|'
                 result[0] = text
+            # if text.find('가능') == -1 and text.find('원산지') == -1 and text.find('표기') == -1 and text.find(
+            #         '용기') == -1 and text.find(
+            #     '개당') == -1 and text.find('g당') == -1 and text.find('기간') == -1 and text.find('추천') == -1 \
+            #         and text.find(',') == -1 and text.find('단위') == -1 and text.find('가격') == -1 and text.find(
+            #     '카드') == -1 \
+            #         and text.find('할인') == -1:
+            #     print("후보 물건 이름!!! : " + text)
+            # if result[0][0]=='|'
+            #     print("후보 물건 이름!!! : " + text)
+
+            # result[0] = text
 
     # print('Texts:')
     #
@@ -58,9 +77,9 @@ def hello():
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
 
-    jsonResponse = {'name': result[0], 'price': result[1]}
+    json_response = {'name': result[0], 'price': result[1]}
 
-    return jsonify(jsonResponse)
+    return jsonify(json_response)
 
 
 def detect_text(path):
