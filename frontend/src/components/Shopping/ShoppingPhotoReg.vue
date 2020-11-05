d05d6f415d387c3ff55f396d6cf96752248482c8
신유빈 <youbin1294@naver.com>
2020-11-05 15:40:28 +0900

    S03P31A301-87 | updated feature | 사진 잘라서 저장하기

--- frontend/src/components/Shopping/ShoppingPhotoReg.vue
+++ frontend/src/components/Shopping/ShoppingPhotoReg.vue
@@ -3,7 +3,7 @@
     <v-btn dark block @click="$router.push({ path: '/shop' })">
       <h2>이전으로 이동</h2>
     </v-btn>
-      <div class="wrapper">
+    <div class="wrapper">
       <video
         class="video"
         :class="facingMode === 'user' ? 'front' : ''"
@@ -20,6 +20,10 @@
       >
         <b-icon pack="fas" icon="sync-alt" />
       </button>
+      <div class="box-border">
+        <h4>가격표를 영역 안에 넣어주세요.</h4>
+        <div class="box"></div>
+      </div>
       <!-- 사진 찍어주는 버튼 -->
       <div class="photo-button-container">
         <v-icon
@@ -29,16 +33,15 @@
           @click="TakePhoto"
         >
           mdi-circle
-          <!-- <b-icon pack="fas" icon="camera" /> -->
         </v-icon>
       </div>
-      <photos-gallery class="gallery" :photos="photos" />
     </div>
   </div>
 </template>
-
-<script>
+  
+  <script>
 import "@/assets/css/components/Shopping/shoppingPhotoReg.scss";
+// import axios from "axios";
 
 export default {
   name: "ShoppingPhotoReg",
@@ -53,7 +56,7 @@ export default {
     async StartRecording(facingMode) {
       this.facingMode = facingMode;
       let video = this.$refs.video;
-      console.log(video);
+      // console.log(video);
       this.mediaStream = await navigator.mediaDevices.getUserMedia({
         video: { facingMode: facingMode },
       });
@@ -65,6 +68,10 @@ export default {
       let canva = this.$refs.canva;
       let width = video.videoWidth;
       let height = video.videoHeight;
+      let dw = width * 0.8;
+      let dh = height * 0.3;
+      let dx = (dw / 2) * -1;
+      let dy = dh / 2;
       canva.width = width;
       canva.height = height;
       let ctx = canva.getContext("2d");
@@ -72,22 +79,81 @@ export default {
 
       if (this.facingMode === "user") {
         ctx.scale(-1, 1);
-        ctx.drawImage(video, width * -1, 0, width, height);
+        // ctx.drawImage(video, width * -1, 0, width, height);
+        ctx.drawImage(
+          video,
+          200,
+          150,
+          dw * 1.2,
+          dh * 1.2,
+          dx,
+          dy,
+          dw * 1.2,
+          dh * 1.2
+        ); // 이미지객체, -dx, dy
+
+        // ctx.drawImage(video, (width / 2) * -1, 0.5, width, height);
       } else {
         ctx.drawImage(video, 0, 0);
       }
       ctx.restore();
-      this.photos.push({
+      this.photo = {
         id: this.counter++,
         src: canva.toDataURL("image/png"),
-      });
-      console.log(this.photos);
+      };
+      console.log(dx, dy, dw, dh, width, height);
+      console.log(this.photo.src);
+
+      // const body = document.querySelector("body");
+      // let canva2 = this.$refs.canva;
+      // let ctx2 = canva2.getContext("2d");
+      // const image = new Image();
+      // //image객체가 생성되어 속성들을 추가할수 있음
+      // image.src = this.photo.src;
+      // body.appendChild(image);
+      // ctx2.drawImage(image, dx, dy, 1, 1, dw, dh);
+
+      // console.log(image, "wpqkf");
+      //   var image = document.getElementById('myImage'),
+      // canvas = document.createElement('canvas'),
+      // ctx = canvas.getContext('2d');
+      // const URL = "http://localhost:5000/fileUpload";
+      // axios
+      //   .post(
+      //     URL,
+      //     { image: this.photo[0].src }
+      //     // {
+      //     //   headers: { "Content-Type": "multipart/form-data" },
+      //     // }
+      //   )
+      //   .then((res) => {
+      //     console.log(res);
+      //   });
+
+      // ctx.drawImage(this.photo, 33, 71, 104, 124, 21, 20, 87, 104);
+      // console.log(this.photo);
+      // var image = new Image();
+      // image.src = this.photo[0].src;
+      // var croppingCoords = {
+      //   x: this.width / 4,
+      //   y: this.height / 4,
+      //   width: this.width / 2,
+      //   height: this.height / 2,
+      // };
+      // var cc = croppingCoords;
+      // var workCan = document.createElement("canvas"); // create a canvas
+      // workCan.width = Math.floor(cc.width); // set the canvas resolution to the cropped image size
+      // workCan.height = Math.floor(cc.height);
+      // var ctx2 = workCan.getContext("2d"); // get a 2D rendering interface
+      // ctx2.drawImage(image, -Math.floor(cc.x), -Math.floor(cc.y)); // draw the image offset to place it correctly on the cropped region
+      // image.src = workCan.toDataURL(); // set the image source to the canvas as a data URL
+      // console.log(image);
     },
     async switchCamera() {
       this.switchingCamera = true;
       const tracks = this.mediaStream.getVideoTracks();
-      console.log(this.mediaStream), "ff";
-      console.log(this.mediaStream.getVideoTracks());
+      // console.log(this.mediaStream), "ff";
+      // console.log(this.mediaStream.getVideoTracks());
       tracks.forEach((track) => {
         track.stop();
       });
@@ -99,7 +165,7 @@ export default {
   },
   data() {
     return {
-      photos: [],
+      photo: [],
       mediaStream: null,
       videoDevices: [],
       facingMode: "environment",
@@ -109,5 +175,4 @@ export default {
   },
 };
 </script>
-
-
+  
