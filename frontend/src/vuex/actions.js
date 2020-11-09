93d4dfe31d1f4791792935eaa02a7c2786400226
신유빈 <youbin1294@naver.com>
2020-11-09 14:15:06 +0900

    S03P31A301-94 | added feature | 로그아웃 기능 구현완료

--- frontend/src/vuex/actions.js
+++ frontend/src/vuex/actions.js
@@ -3,27 +3,29 @@ import router from "@/router/index";
 import SERVER from "@/api/spring";
 
 export default {
-    postuserData({ commit }, info) {
-        axios
-          .post(info.location, info.data)
-          .then((res) => {
-            commit("SET_USERID", res.data.userid);
-            // sessionStorage에 유저의 정보를 저장
-            sessionStorage.setItem("userid", res.data.userid);
-            router.push({ path: "/perparation" });
-          })
-          .catch(() => alert("아이디 또는 비밀번호가 틀렸습니다"));
-      },
-    signup({ dispatch }, signupData) {
-        const info = {
-          location: SERVER.URL + SERVER.ROUTES.users,
-          data: signupData,
-        };
-        dispatch("postuserData", info);
-    },
-    logout({ commit }) {
-        commit("SET_USERID", null);
-        router.push({path: "/"})
+  postuserData({ commit }, info) {
+      axios
+        .post(info.location, info.data)
+        .then((res) => {
+          commit("SET_USERID", res.data.userid);
+          // sessionStorage에 유저의 정보를 저장
+          sessionStorage.setItem("userid", res.data.userid);
+          router.push({ path: "/perparation" });
+        })
+        .catch(() => alert("아이디 또는 비밀번호가 틀렸습니다"));
+  },
+  signup({ dispatch }, signupData) {
+      const info = {
+        location: SERVER.URL + SERVER.ROUTES.users,
+        data: signupData,
+      };
+      dispatch("postuserData", info);
+  },
+  logout({ commit }) {
+    commit("SET_USERID", null);
+    alert("로그아웃 되었습니다.")
+    sessionStorage.removeItem("userid");
+    router.push({path: "/"})
   },
   login({ dispatch }, loginData) {
     const info = {
@@ -31,7 +33,5 @@ export default {
       data: loginData,
     };
     dispatch("postuserData", info);
-      
-    }
-  
+  }
 }
