509720d2f88c2a7d7f4d4be2b0d13e85964af25e
Kim Chan Young <ghkdua059@gmail.com>
2020-11-12 10:20:39 +0900

    S03P31A301-86 | updated feature | 사진 분석 예외처리

--- backend/flaskr/app/Ocr.py
+++ backend/flaskr/app/Ocr.py
@@ -1,5 +1,6 @@
 """Detects text in the file."""
 from google.cloud import vision_v1 as vision3
+from flask_api import status
 import io
 import re
 from flask import Flask, request, jsonify
@@ -29,8 +30,10 @@ def hello():
     response = client.text_detection(image=image)
     texts = response.text_annotations
     # -*- Encoding:UTF-8 -*- #
+    if (len(texts) == 0):
+        content = {'status': 400, 'message': '사진에서 분석할 글자가 없습니다.'}
+        return content, status.HTTP_400_BAD_REQUEST
     text_arr = texts[0].description.split("\n")
-    # s = '123abc.-30=*/-'
 
     limit_texts = ['가능', '원산지', '표기', '용기', '개당', 'g당', '기간', '추천', ',', '단위', '가격', '카드', '할인', '마트', '박스', '그대로',
                    '온라인몰', '행사']
@@ -39,7 +42,6 @@ def hello():
     for text in text_arr:
         str_num = "".join(re.findall('\d+', text))
         hangul_result = hangul.sub('', text)  # 한글과 띄어쓰기를 제외한 모든 부분을 제거
-        # print("[" + str_num + "]")
         if len(str_num) != 0:
 
             if len(hangul_result) < 3 and 100 < int(str_num) < 500000 and text.find('/') == -1:
@@ -59,24 +61,6 @@ def hello():
             if token is False:
                 print("후보 물건 이름!!! : " + text)
                 result[0] = text
-            # if text.find('가능') == -1 and text.find('원산지') == -1 and text.find('표기') == -1 and text.find(
-            #         '용기') == -1 and text.find(
-            #     '개당') == -1 and text.find('g당') == -1 and text.find('기간') == -1 and text.find('추천') == -1 \
-            #         and text.find(',') == -1 and text.find('단위') == -1 and text.find('가격') == -1 and text.find(
-            #     '카드') == -1 \
-            #         and text.find('할인') == -1:
-            #     print("후보 물건 이름!!! : " + text)
-            # if result[0][0]=='|'
-            #     print("후보 물건 이름!!! : " + text)
-
-            # result[0] = text
-
-    # print('Texts:')
-    #
-    # for text in texts:
-    #     content = text.description
-    #     content = content.replace(',', '')
-    #     print('\n"{}"'.format(content))
 
     if response.error.message:
         raise Exception(
@@ -84,7 +68,11 @@ def hello():
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
 
-    json_response = {'name': result[0], 'price': result[1]}
+    json_response = {'status': 200, 'name': result[0], 'price': result[1]}
+
+    if (len(result[0]) == 0 or len(result[1]) == 0):
+        content = {'status': 400, 'message': '물건 이름 혹은 가격을 인식할 수 없습니다.'}
+        return content, status.HTTP_400_BAD_REQUEST
 
     return jsonify(json_response)
 
