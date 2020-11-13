1e0223f30f775dd2c668dddf81e6ebb1be8dbed7
신유빈 <youbin1294@naver.com>
2020-11-13 11:31:00 +0900

    S03P31A301-39 | updated feature | grade 연결

--- frontend/src/vuex/state.js
+++ frontend/src/vuex/state.js
@@ -11,8 +11,8 @@ export default {
     },
     gradeImgae: [
         "",
-        "https://user-images.githubusercontent.com/60081201/99012095-ef957580-2590-11eb-86f5-5d4b96060252.PNG",
         "https://user-images.githubusercontent.com/60081201/99012097-f0c6a280-2590-11eb-8cd2-61c85c1b78f8.PNG",
-        "https://user-images.githubusercontent.com/60081201/99012098-f15f3900-2590-11eb-95b1-f526d13b44b7.PNG"
+        "https://user-images.githubusercontent.com/60081201/99012098-f15f3900-2590-11eb-95b1-f526d13b44b7.PNG",
+        "https://user-images.githubusercontent.com/60081201/99012095-ef957580-2590-11eb-86f5-5d4b96060252.PNG",
     ] // rawpixel.com - kr.freepik.com가 제작함
 }
